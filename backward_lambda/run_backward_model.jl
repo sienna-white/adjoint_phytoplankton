c736b53b1b25263fbcf0bfd6784eea80817e0b2d
Sienna White <siennarwhite@gmail.com>
2025-04-15 15:35:10 -0700

    first push... weird oscillations in adj rn.

---
+++ backward_lambda/run_backward_model.jl
@@ -0,0 +1,154 @@
+#!/usr/bin/env julia
+
+using Printf
+using DataStructures: OrderedDict
+using NCDatasets
+using Arrow, DataFrames
+using CSV, DataFrames
+using Colors
+using ColorSchemes
+using Plots
+using Printf
+using LaTeXStrings
+using Profile
+using Statistics 
+
+include("../run_hydro/calculate_physical_variables.jl") 
+include("../run_hydro/advance_variables.jl")
+include("../run_hydro/phytoplankton.jl")
+include("../run_hydro/forcings.jl") 
+include("../run_hydro/output.jl")
+
+
+file_out_name = "adjoint.nc" 
+
+
+using Random
+Random.seed!(1234);      # Seed number 1234
+
+
+
+function run_my_model(file_out_name::String)
+
+    ds_hydro = NCDataset("../run_hydro/HYDRO.nc")
+    ds_algae = NCDataset("../forward_phyto/phyto_GUESS.nc")
+    ds_truth = NCDataset("../forward_phyto/phyto_TRUTH.nc")
+    
+
+    # println(ds_hydro) 
+    # println(ds_algae)
+
+
+
+    #********************** SPATIAL DOMAIN  ***************************
+    N = 60    # number of grid points
+    H = 6    # depth (meters)
+    dz = H/N  # grid spacing - may need to adjust to reduce oscillations
+    dt = 10   # (seconds) size of time step 
+    M  = 10000 #00 #000 # 50000  #500 #
+
+    # Increments for saving profiles. set to 1 to save all; 10 saves every 10th, etc. 
+    isave = 1 #1000
+    var2save = ["lambda"]
+
+    create_output_dict(M, isave, var2save, N)
+
+    # Create depth vector 
+    z = collect(H:-dz:dz) .- dz/2
+
+    # Swimming speed 
+    ws = 1.38e-4
+
+    lambda = zeros(N) 
+
+    println("Initializing with ground truth + some noise")
+    ground_truth_w_noise =  ds_truth["algae1"][:,end] #+ rand(N).*1e-3
+    c_diff = 2*(ds_algae["algae1"][:,end] - ground_truth_w_noise)
+    # c_diff = reverse(c_diff)
+    # println(c_diff)
+
+    # println(ds_truth['z'][:])
+
+    # assert(false)
+
+    L_n = c_diff
+
+    # print(c_diff)
+
+    save2output(time, M-1, "lambda", L_n) 
+
+    Times = collect(0:dt:(M*dt))
+    Times = Times[1:end-1]
+
+
+    for i in (M-2):-1:1
+        println("Time = $i") 
+
+        time = Times[i];
+
+        #  If settling speed is UPWARD (swimming!)
+        gamma = ds_algae["gamma"][:,i]
+        kz    = ds_hydro["Kz"][:,i]
+        aL, bL, cL, dL = initialize_abcd(N)
+         
+        if ws>0
+            for i in 2:(N-1)
+                aL[i] = ws*dt/dz - (dt/dz^2)*(1/2)*(kz[i-1] + kz[i])
+                bL[i] = 1 - ws*dt/dz - gamma[i]*dt + (dt/dz^2)*(1/2)*(kz[i+1] + 2*kz[i] + kz[i-1])
+                cL[i] = - (dt/dz^2)*(1/2) * (kz[i] + kz[i+1])
+                dL[i] = L_n[i] 
+            end
+        end 
+
+        # Bottom-Boundary: no flux for scalars
+        bL[1] =  1 - (gamma[1]*dt) + (dt/dz^2)*(1/2)*(kz[1] + kz[2]) 
+        cL[1] =  -(dt/dz^2) * (1/2) * (kz[1] + kz[2])
+        dL[1] =  L_n[1]
+
+        # Top-Boundary: no flux for scalars
+        aL[end] = ws*dt/dz - (dt/dz^2)*(1/2)* (kz[end] + kz[end-1])
+        bL[end] = 1 - gamma[end]*dt + (dt/dz^2)*(1/2)*(kz[end] + kz[end-1])  
+        dL[end] = L_n[end]
+
+        L_nminus1 = TDMA(aL, bL, cL, dL, N) 
+
+        save2output(time, i, "lambda", L_nminus1)
+        L_n = L_nminus1
+    end 
+
+
+#     # ********************** save data ****************************
+    units_dict = Dict("lambda" => "[-]")
+
+    var2name = Dict("lambda" => "Lagrangian multiplier")
+
+    times_unique = unique(times[1:end]) 
+    
+
+    print("Times unique has $(length(times_unique)) elements \n")
+
+
+    ds = NCDataset(file_out_name,"c")
+    defDim(ds, "z", length(z)) 
+    defDim(ds, "time", length(times_unique))
+
+    v = defVar(ds, "z", Float32, ("z",))
+    v[:] = z
+
+    v = defVar(ds, "time", Float32, ("time",), attrib = OrderedDict("units" => "seconds"))
+    v[:] = collect(1:(length(times_unique)))
+
+    for var in var2save
+        v = defVar(ds, var, Float64,("z","time"), attrib = OrderedDict(
+        "units" =>  units_dict[var], "long_name" => var2name[var]))
+        v[:,:] = output[var];
+    end
+
+    print("Saved $file_out_name \n")
+    close(ds)
+
+
+
+end 
+
+run_my_model(file_out_name)
---
+++ forward_phyto/run_forward_model.jl
@@ -0,0 +1,201 @@
+#!/usr/bin/env julia
+
+using Plots
+using Printf
+using DataStructures: OrderedDict
+using NCDatasets
+using Arrow, DataFrames
+using CSV, DataFrames
+using Colors
+using ColorSchemes
+using Plots
+using Printf
+using LaTeXStrings
+using Profile
+using Statistics 
+
+include("calculate_physical_variables.jl") 
+include("advance_variables.jl")
+include("phytoplankton.jl")
+include("forcings.jl") 
+include("output.jl")
+
+
+
+
+
+file_out_name = "phyto_GUESS.nc" 
+
+function run_my_model(file_out_name::String)
+
+    ds = NCDataset("../run_hydro/HYDRO.nc")
+
+
+    #***********************************************************************
+    # Read in the CIMIS data
+    cimis_fn = "/global/homes/s/siennaw/scratch/siennaw/turbulence-model/data/CIMIS/PAR_on_august_9-15.csv"
+    df = CSV.read(cimis_fn, DataFrame)
+    par = df[!,"Sol Rad (PAR)"]
+    println("Read in CIMIS data ...")
+
+
+    function get_light(index::Int, par=par)
+        return par[index]
+    end
+
+    #********************** SPATIAL DOMAIN  ***************************
+    N = 60    # number of grid points
+    H = 6    # depth (meters)
+    dz = H/N  # grid spacing - may need to adjust to reduce oscillations
+    dt = 10   # (seconds) size of time step 
+    M  = 10000 #00 #000 # 50000  #500 #
+
+    # Increments for saving profiles. set to 1 to save all; 10 saves every 10th, etc. 
+    isave = 1 
+    var2save = ["algae1", "gamma"]
+
+    create_output_dict(M, isave, var2save, N)
+
+    # Create depth vector 
+    z = collect(H:-dz:dz) .- dz/2 # depth vector
+    # println("Length of z is ", length(z))
+
+    #********************** FIXED CONSTANTS  ***************************
+    rhoA = 1.23                     # Density of air, kg/m^3
+    rhoW = 1000                     # Density of water, kg/m^3
+    specific_heat_water = 4181      # J/kg-degC
+    specific_heat_air = 1007        # J/kg-degC x RH
+    c_d = 0.05                      # Drag coefficient [-]
+    cm2m = 0.01
+    hr2s = 1/3600
+
+    # (4) Light 
+    DIURNAL_LIGHT = false  
+    background_turbidity =  0.6
+    I_in = 350 
+
+    #********************** DEFINE PHYTOPLANKTON FORCINGS ***************************
+    init_algae = 0.01
+
+    algae1 = Dict("k" => 0.034,              # specific light attenuation coefficient [cm^2 / 10^6 cells]
+                "pmax" => 0.008 * hr2s,           # maximum specific growth rate [1/hour]
+                "ws" => 1.38e-4,           # vertical velocity [m/s]
+                "Hi" => 40,                # half-saturation of light-limited growth [mu mol photons * m^2/s]
+                "Li" => 0.004 * hr2s,             # specific loss rate [1/hour]
+                "name" => "HAB",           # name of the species
+                "self_shading" => true)    # self-shading effect (true/false)
+    # '''
+    # Diatoms ws = -1.38e-5 m/s
+    # Cyanobacteria = 1.38e-4 m/s
+    # '''
+
+    #***************************************************************************
+    #   Initialize variables
+    #***************************************************************************
+
+    # Create dictionary to hold important discretization parameters
+    discretization = Dict("beta" => (dt/dz^2), "dz" => dz, "dt" => dt, "N" => N, "z"=> z, "H" => H)
+
+    algae1["c"] = zeros(N) .+ init_algae 
+    # algae2["c"] = zeros(N) .+ 1e-3 #@init_algae 
+
+    Times = collect(0:dt:(M*dt))
+
+    # println("Times = ", Times)
+
+    real_times_saved = []
+
+    #***************************************************************************
+    save2output(1, 1, "algae1", algae1["c"])
+    # save2output(1, 1, "algae2", algae2["c"])
+
+    variables = Dict("Kz" => ds["Kz"][:,1])
+
+    for i in 2:(M-1)
+
+        time = Times[i];
+
+        I0 = get_light(i)
+
+        # [8] Advance phytoplankton
+        # light = self_shading(algae1, algae2, I0, background_turbidity, discretization)
+        light = light_decay(I0, background_turbidity, discretization)
+
+        # Hydrodynamics
+        variables["Kz"] = ds["Kz"][:,i]
+
+        # Algae 1
+        gamma = calculate_net_growth(algae1, light, discretization)
+        algae1["c"] = advance_algae(variables, algae1, gamma, discretization)  # zeros(N) .+ init_algae  #
+        # println("Algae 1: ", algae1["c"])
+        # Algae 2
+        # gamma = calculate_net_growth(algae2, light, discretization) 
+        # algae2["c"] = advance_algae(variables, algae2, gamma, discretization)
+        # algae2["c"] = zeros(N) .+ init_algae 
+
+        # [9] Pack variables for next timestep 
+        
+
+        save2output(time, i, "gamma", gamma)
+        save2output(time, i, "algae1", algae1["c"])
+        # save2output(time, i, "algae2", algae2["c"])
+            # push!(real_times_saved, real_time[i])
+        
+
+    end
+
+
+
+    # ********************** save data ****************************
+    units_dict = Dict("U" => "m/s", 
+        "C" => "deg C", 
+        "Kz" => "m\$^2\$ s\$^{-1}\$", 
+        "algae1" => L"10$^6$/cm$^3$ cells",
+        "algae2" => L"10$^6$/cm$^3$ cells",
+        "L" => "Turbulent length scale", 
+        "Q2" => "TKE", "Q2L" => "TKE*L",
+        "N_BV2" => "Brunt-Vaisala frequency", 
+        "Kq" => "Kq", 
+        "Nu" => "Nu_t",
+        "gamma" => "Net growth rate",)
+
+    var2name = Dict("U" => "Velocity", 
+                "C" => "Temperature", 
+                "Kz" => "Turbulent diffusivity", 
+                "algae1" => "Diatom concentration",
+                "algae2" => "HAB concentration",
+                "L" => "Turbulent length scale", 
+                "Q2" => "TKE","Q2L" => "TKE*L",
+                "N_BV2" => "Brunt-Vaisala frequency", "Kq" => "Kq", "Nu" => "Nu_t", "gamma" => "Net growth rate [1/s]")
+
+    times_unique = unique(times) 
+
+    ds = NCDataset(file_out_name,"c")
+    defDim(ds, "z", length(z)) 
+    defDim(ds, "time", length(times_unique))
+
+    v = defVar(ds, "z", Float32, ("z",))
+    v[:] = z
+
+    v = defVar(ds, "time", Float32, ("time",), attrib = OrderedDict("units" => "seconds"))
+    v[:] = collect(1:(length(times_unique))) #model_time
+
+    for var in var2save
+        # println(var)
+        v = defVar(ds, var, Float64,("z","time"), attrib = OrderedDict(
+        "units" =>  units_dict[var], "long_name" => var2name[var]))
+        v[:,:] = output[var];
+    end
+
+    print("Saved $file_out_name \n")
+    close(ds)
+    
+end 
+
+
+run_my_model(file_out_name)
+
+# @profilehtml run_my_model(ws1, ws2, pmax1, pmax2, file_out_name)
+
+# StatProfilerHTML.view()
+# Profile.print() 
